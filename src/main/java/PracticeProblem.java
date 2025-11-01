fc1769a21d0127f0891a459838dbe0eab3144e96
kyler-huang-ics4u1-01-25 <kyler.huang@student.tdsb.on.ca>
2025-11-01 19:19:30 +0000

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,53 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+    public static void main(String[] args) {
+        String[] names = {"Jacob", "Eric", "Tiffany", "Alice"};
+        int[] ages = {17, 17, 17, 16};
 
-	}
+        selectionSortName(names, ages);
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i] + " " + ages[i]);
+        }
 
-	
+        selectionSortAge(names, ages);
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i] + " " + ages[i]);
+        }
+    }
 
+    public static void selectionSortName(String[] names, int[] ages) {
+        for (int i = 0; i < names.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < names.length; j++) {
+                if (names[j].compareToIgnoreCase(names[minIndex]) < 0) {
+                    minIndex = j;
+                }
+            }
+            String tempName = names[i];
+            names[i] = names[minIndex];
+            names[minIndex] = tempName;
+
+            int tempAge = ages[i];
+            ages[i] = ages[minIndex];
+            ages[minIndex] = tempAge;
+        }
+    }
+
+    public static void selectionSortAge(String[] names, int[] ages) {
+        for (int i = 0; i < ages.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < ages.length; j++) {
+                if (ages[j] < ages[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int tempAge = ages[i];
+            ages[i] = ages[minIndex];
+            ages[minIndex] = tempAge;
+
+            String tempName = names[i];
+            names[i] = names[minIndex];
+            names[minIndex] = tempName;
+        }
+    }
 }
